de3e05ff6038467653a9a20b0bed23132c6b0a8d
Laurent Bigonville <bigon@bigon.be>
2012-09-19 17:57:25 +0200

    Imported Upstream version 1.7.11

--- auparse/ellist.c
+++ auparse/ellist.c
@@ -160,17 +160,23 @@ static int parse_up_record(rnode* r)
 			}
 			// Make virtual keys or just store it
 			if (strcmp(n.name, "key") == 0 && *n.val != '(') {
-				char *key = (char *)au_unescape(n.val);
-				char *ptr = strtok_r(key, key_sep, &saved);
-				free(n.name);
-				free(n.val);
-				while (ptr) {
-					n.name = strdup("key");
-					n.val = escape(ptr);
+				if (*n.val == '"')
 					nvlist_append(&r->nv, &n);
-					ptr = strtok_r(NULL, key_sep, &saved);
+				else {
+					char *key = (char *)au_unescape(n.val);
+					char *ptr = strtok_r(key,
+							key_sep, &saved);
+					free(n.name);
+					free(n.val);
+					while (ptr) {
+						n.name = strdup("key");
+						n.val = escape(ptr);
+						nvlist_append(&r->nv, &n);
+						ptr = strtok_r(NULL,
+							key_sep, &saved);
+					}
+					free(key);
 				}
-				free(key);
 				continue;
 			} else
 				nvlist_append(&r->nv, &n);
